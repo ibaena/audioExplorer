cfb74e1096c19380998e9b83d9a2edde64ab9680
Ivan <ivan.baena04@gmail.com>
2016-05-15 12:41:55 -0400

    test

--- client/audioExplorer/Player.jsx
+++ client/audioExplorer/Player.jsx
@@ -13,7 +13,6 @@ export default class Player extends Component {
   }
 
  componentDidMount(){
-   console.log(this.state);
    Meteor.call("streamRadio", {query: this.state.urlParam },(error, res) => {
       if(error) {
         console.log(error);
@@ -26,17 +25,32 @@ export default class Player extends Component {
     });
  }
 
-render(){
-  return(
-    <div>
-        {this.state.stream.map(radio => {
-          return <audio key={radio.id} src={radio.stream} controls="controls"></audio>
+render() {
+  return (
+    <div id="player">
+      <div className="col s12 l6">
+        <ul className="collection">
+          {this.state.stream.map(radio => {
+            return <li key={radio.playingnow.album_id} className="collection-item avatar">
+              <i className="material-icons circle blue">radio</i>
+              <span className="title">{radio.playingnow.track_name}</span>
+              <p>{radio.playingnow.artist_name}
+                <br/> {radio.playingnow.artist_name}
+              </p>
+            </li>
 
+          })
+}
+        </ul>
+      </div>
+      <div className="col s12 l6">
+        {this.state.stream.map(radio => {
+          return <audio key={radio.id} autoplay="autoplay" src={radio.stream} controls="controls"></audio>
         })
-      }
+}
+      </div>
     </div>
   )
 }
-
 
 }
